79b7459c41a3360321a0f8bd6ce54959be16420e
Felipe <felipecezaralmeida0@gmail.com>
2026-03-10 11:00:16 -0300

    Adicionando funções do CRUD (buscar e atualizar

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ class Produto(Base):
         return f"Produto(id={self.id}, nome='{self.nome}', preco={self.preco}, estoque={self.estoque}, ativo={self.ativo})"
     
 #Criar conexão com o banco de dados SQLite
-engine = create_engine("sqlite:///estoque.db", echo=True)
+engine = create_engine("sqlite:///estoque.db", echo=False)
 
 #Criar as tabelas no banco de dados, se ainda não existirem
 Base.metadata.create_all(engine)
@@ -72,14 +72,14 @@ produto1 = Produto("Notebook", 5500, 6, True)
 produto2 = Produto("Teclado", 500, 100, True)
 produto3 = Produto("Mouse", 150, 55, True)
 
-#Adicionar os produtos na sessão (carrinho)
-session.add(produto1)
-session.add(produto2)
-session.add(produto3)
+# #Adicionar os produtos na sessão (carrinho)
+# session.add(produto1)
+# session.add(produto2)
+# session.add(produto3)
 
-#Confirmar a inserção no banco
-#Salvar no banco de dados
-session.commit()
+# #Confirmar a inserção no banco
+# #Salvar no banco de dados
+# session.commit()
 
 #Listar
 #Buscar todos os produtos do banco
@@ -89,3 +89,42 @@ print(produtos)
 
 for p in produtos:
     print( f"Produto(id={p.id}, nome='{p.nome}', preco={p.preco}, estoque={p.estoque}, ativo={p.ativo})")
+
+
+# #UPDATE (Atualizar)
+
+# #Buscar o produto com o id = 1
+# produto_id = session.query(Produto).filter(Produto.id == 1).first()
+# print(produto_id)
+
+# #Produtos maior que 10
+# num_estoque = session.query(Produto).filter(Produto.estoque > 10).all()
+# for produto in num_estoque:
+#  print(produto.estoque)
+
+# produto_id2 = session.query(Produto). filter_by(id = 1).first()
+# #print(produto_id2)
+
+# #Podemos usar order by
+# produtos_organizados = session.query(Produto).order_by(Produto.estoque).all()
+# #for produto in produtos_organizados:
+#    #print(f"Nome:{produto.nome}, Qtd_estoque: {produto.estoque}")
+
+# #Podemos limitar a quantidade de resultado - top 5 mais caros
+# produtos_mais_caros = session.query(Produto).order_by(Produto.preco.desc()).limit(3).all()
+# for produto in produtos_mais_caros:
+#    print(f"Nome:{produto.nome}, Valor: {produto.preco}")
+
+
+#UPDATE - Atualizar
+#Busquei o produo para atualizar
+notebook = session.query(Produto).filter_by(id=1).first
+notebook.preco =  6000
+
+#Confirmar essa alteração
+session.commit()
+
+print("Preço atualizado com sucesso")
+
+for p in produtos:
+   print(  f"Produto(id={p.id}, nome='{p.nome}', preco={p.preco}, estoque={p.estoque}, ativo={p.ativo})")
